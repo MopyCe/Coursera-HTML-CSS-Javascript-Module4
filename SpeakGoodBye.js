b83079fa48fcc163b24019d60e6f72b3ad7788e8
MopyCe <92428668+MopyCe@users.noreply.github.com>
2021-10-28 17:00:46 +0200

    Solution to module 4

---
+++ SpeakGoodBye.js
@@ -0,0 +1,15 @@
+
+( function() {
+var speakWord = "Good Bye";
+
+(function (window) {
+  var byeSpeaker = {};
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  }
+
+  window.byeSpeaker = byeSpeaker;
+
+})(window);
+
+})();
---
+++ SpeakHello.js
@@ -0,0 +1,13 @@
+( function() {
+var speakWord = "Hello";
+
+(function (window) {
+  var helloSpeaker = {};
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  }
+
+  window.helloSpeaker = helloSpeaker;
+
+})(window);
+})();
